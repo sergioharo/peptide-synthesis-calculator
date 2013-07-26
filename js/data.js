c6699fe5db2a8aef6292f324b45898e7a54bd477
Sergio Haro <sergio.haro.jr@gmail.com>
2013-07-26 10:07:18 -0400

    amino acid data

---
+++ js/data.js
@@ -0,0 +1,116 @@
+define([], function() {
+	var aa = {
+		A: {
+			type: "amino-acid",
+			name: "Fmoc-L-Ala",
+			mw: 311.34
+		},
+		C: {
+			type: "amino-acid",
+			name: "Fmoc-L-Cys (Trt)",
+			mw: 585.74
+		},
+		D: {
+			type: "amino-acid",
+			name: "Fmoc-L-Asp(OtBu)",
+			mw: 411.45
+		},
+		E: {
+			type: "amino-acid",
+			name: "Fmoc-L-Glu(OtBu)-OH",
+			mw: 425.5
+		},
+		F: {
+			type: "amino-acid",
+			name: "Fmoc-L-Phe",
+			mw: 387.43
+		},
+		G: {
+			type: "amino-acid",
+			name: "Fmoc-L-Gly-OH",
+			mw: 297.3
+		},
+		H: {
+			type: "amino-acid",
+			name: "Fmoc-L-His(Trt)",
+			mw: 619.7
+		},
+		I: {
+			type: "amino-acid",
+			name: "Fmoc-L-Ile",
+			mw: 353.4
+		},
+		K: {
+			type: "amino-acid",
+			name: "Fmoc-L-Lys(Boc)-OH",
+			mw: 468.5
+		},
+		L: {
+			type: "amino-acid",
+			name: "Fmoc-L-Leu",
+			mw: 353.4
+		},
+		M: {
+			type: "amino-acid",
+			name: "Fmoc-L-Met",
+			mw: 371.5
+		},
+		N: {
+			type: "amino-acid",
+			name: "Fmoc-L-Asn(Trt)-OH",
+			mw: 596.68
+		},
+		P: {
+			type: "amino-acid",
+			name: "Fmoc-L-Pro",
+			mw: 337.4
+		},
+		Q: {
+			type: "amino-acid",
+			name: "Fmoc-L-Gln(Trt)-OH",
+			mw: 610.7
+		},
+		R: {
+			type: "amino-acid",
+			name: "Fmoc-L-Arg(Pbf)-OH",
+			mw: 648.8
+		},
+		S: {
+			type: "amino-acid",
+			name: "Fmoc-L-Ser(tBu)-OH",
+			mw: 383.4
+		},
+		T: {
+			type: "amino-acid",
+			name: "Fmoc-L-Thr(tBu)",
+			mw: 397.5
+		},
+		V: {
+			type: "amino-acid",
+			name: "Fmoc-L-Val",
+			mw: 339.4
+		},
+		W: {
+			type: "amino-acid",
+			name: "Fmoc-L-Trp(Boc)-OH",
+			mw: 526.6
+		},
+		Y: {
+			type: "amino-acid",
+			name: "Fmoc-L-Tyr(tBu)",
+			mw: 459.54
+		},
+		HATU: {
+			type: "activator",
+			name: "",
+			mw: 380.23
+		},
+		HBTU: {
+			type: "activator",
+			name: "",
+			mw: 379.3
+		}
+	};
+
+	return aa;
+});
