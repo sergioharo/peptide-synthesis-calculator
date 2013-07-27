fa506b931617f0db7468043740e684b3fef2e648
Sergio Haro <sergio.haro.jr@gmail.com>
2013-07-27 00:42:30 -0400

    Exposing additional settings

--- js/views/settingsView.js
+++ js/views/settingsView.js
@@ -1,4 +1,4 @@
-define(["jquery", "backbone", "underscore"], function ($, Backbone, _) {
+define(["backbone", "underscore"], function (Backbone, _) {
 
 	var settingsView = Backbone.View.extend({
 
@@ -10,28 +10,43 @@ define(["jquery", "backbone", "underscore"], function ($, Backbone, _) {
 		},
 
 		render: function() {
-			var template = _.template( $(this.template).html(), { 
-				val: this.model.get("amount")
+			var template = _.template( $(this.template).html(), {
+				amt: this.model.get("amount"),
+				equiv: this.model.get("amino-acid"),
+				actEquiv: this.model.get("activator")
 			});
 			this.$el.html( template );
 			return this;
 		},
 
 		events: {
-			'change .amtInput': 'amountChanged'
+			'change .amount': 'amountChanged',
+			'change .equiv': 'equivChanged',
+			'change .actEquiv': 'actEquivChanged'
 		},
 
-		amountChanged: function (e) {
-			var amt = this.$('.amtInput').val();
+		valChanged: function (input, propName)
+		{
+			var amt = this.$(input).val();
 
 			amt = parseFloat(amt);
 
-			if (!amt || amt == NaN)
+			if (!amt || isNaN(amt))
 				return false;
 
-			this.model.set({
-				"amount": amt
-			});
+			this.model.set(propName, amt);
+		},
+
+		amountChanged: function (e) {
+			this.valChanged(".amount", "amount");
+		},
+
+		equivChanged: function (e) {
+			this.valChanged(".equiv", "amino-acid");
+		},
+
+		actEquivChanged: function (e) {
+			this.valChanged(".actEquiv", "activator");
 		}
 
 	});
