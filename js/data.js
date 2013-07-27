5a8329fc1bbd155f1eed0bb7e0cd4223fd16e078
Sergio Haro <sergio.haro.jr@gmail.com>
2013-07-27 01:29:08 -0400

    Adding code property to data file

--- js/data.js
+++ js/data.js
@@ -1,111 +1,133 @@
 define([], function() {
 	var aa = {
 		A: {
+			code: "A",
 			type: "amino-acid",
 			name: "Fmoc-L-Ala",
 			mw: 311.34
 		},
 		C: {
+			code: "C",
 			type: "amino-acid",
 			name: "Fmoc-L-Cys (Trt)",
 			mw: 585.74
 		},
 		D: {
+			code: "D",
 			type: "amino-acid",
 			name: "Fmoc-L-Asp(OtBu)",
 			mw: 411.45
 		},
 		E: {
+			code: "E",
 			type: "amino-acid",
 			name: "Fmoc-L-Glu(OtBu)-OH",
 			mw: 425.5
 		},
 		F: {
+			code: "F",
 			type: "amino-acid",
 			name: "Fmoc-L-Phe",
 			mw: 387.43
 		},
 		G: {
+			code: "G",
 			type: "amino-acid",
 			name: "Fmoc-L-Gly-OH",
 			mw: 297.3
 		},
 		H: {
+			code: "H",
 			type: "amino-acid",
 			name: "Fmoc-L-His(Trt)",
 			mw: 619.7
 		},
 		I: {
+			code: "I",
 			type: "amino-acid",
 			name: "Fmoc-L-Ile",
 			mw: 353.4
 		},
 		K: {
+			code: "K",
 			type: "amino-acid",
 			name: "Fmoc-L-Lys(Boc)-OH",
 			mw: 468.5
 		},
 		L: {
+			code: "L",
 			type: "amino-acid",
 			name: "Fmoc-L-Leu",
 			mw: 353.4
 		},
 		M: {
+			code: "M",
 			type: "amino-acid",
 			name: "Fmoc-L-Met",
 			mw: 371.5
 		},
 		N: {
+			code: "N",
 			type: "amino-acid",
 			name: "Fmoc-L-Asn(Trt)-OH",
 			mw: 596.68
 		},
 		P: {
+			code: "P",
 			type: "amino-acid",
 			name: "Fmoc-L-Pro",
 			mw: 337.4
 		},
 		Q: {
+			code: "Q",
 			type: "amino-acid",
 			name: "Fmoc-L-Gln(Trt)-OH",
 			mw: 610.7
 		},
 		R: {
+			code: "R",
 			type: "amino-acid",
 			name: "Fmoc-L-Arg(Pbf)-OH",
 			mw: 648.8
 		},
 		S: {
+			code: "S",
 			type: "amino-acid",
 			name: "Fmoc-L-Ser(tBu)-OH",
 			mw: 383.4
 		},
 		T: {
+			code: "T",
 			type: "amino-acid",
 			name: "Fmoc-L-Thr(tBu)",
 			mw: 397.5
 		},
 		V: {
+			code: "V",
 			type: "amino-acid",
 			name: "Fmoc-L-Val",
 			mw: 339.4
 		},
 		W: {
+			code: "W",
 			type: "amino-acid",
 			name: "Fmoc-L-Trp(Boc)-OH",
 			mw: 526.6
 		},
 		Y: {
+			code: "Y",
 			type: "amino-acid",
 			name: "Fmoc-L-Tyr(tBu)",
 			mw: 459.54
 		},
 		HATU: {
+			code: "HATU",
 			type: "activator",
 			name: "HATU",
 			mw: 380.23
 		},
 		HBTU: {
+			code: "HBTU",
 			type: "activator",
 			name: "HBTU",
 			mw: 379.3
